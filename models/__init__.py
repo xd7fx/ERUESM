64d83dadf28416d4e163f5c659e2dd783ccd47fd
Abdulrahman Alnashri <d7.fx99@gmail.com>
2024-12-03 09:10:25 +0300

    Update __init__.py

--- models/__init__.py
+++ models/__init__.py
@@ -1,7 +1,3 @@
 print("Importing AuViLSTMModel...")
 from .auvi_lstm_model import AuViLSTMModel
 print("AuViLSTMModel imported successfully!")
-
-print("Importing EmotionRecognizer...")
-from .emotion_recognizer import EmotionRecognizer
-print("EmotionRecognizer imported successfully!")
